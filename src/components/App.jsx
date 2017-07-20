cafc29a0fa6841f81d901e5eb9004a0f2b77a366
Hack Reactor Student <student@hackreactor.com>
2017-07-19 17:04:54 -0700

    onClick function sets video in videoplayer view

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,35 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {videos: window.exampleVideoData, video: window.exampleVideoData[0] };
+    this.handleTitleClick = this.handleTitleClick.bind(this);
+  }
+  handleTitleClick(e, video) {
+    console.log(e);
+    this.setState({video: video});
+  }
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><h5><em>search</em> view goes here</h5></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            {/* <div><h5><em>videoPlayer</em> view goes here</h5></div>*/}
+            <VideoPlayer video={this.state.video}/>
+          </div>
+          <div className="col-md-5">
+            {/*<div><h5><em>videoList</em> view goes here</h5></div>*/}
+            <VideoList handleTitleClick={this.handleTitleClick} videos={this.state.videos}/>
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        {/* <div><h5><em>videoPlayer</em> view goes here</h5></div>*/}
-        <VideoPlayer video={window.exampleVideoData[0]}/>
-      </div>
-      <div className="col-md-5">
-        {/*<div><h5><em>videoList</em> view goes here</h5></div>*/}
-        <VideoList videos={window.exampleVideoData}/>
-      </div>
-    </div>
-  </div>
-);
-
+    );
+  }
+}
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.App = App;
--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -4,7 +4,7 @@ var VideoListEntry = (props) => (
       <img className="media-object" src={props.video.snippet.thumbnails.default.url} alt="" />
     </div>
     <div className="media-body">
-      <div className="video-list-entry-title">{props.video.snippet.title}</div>
+      <div className="video-list-entry-title" onClick={(e)=>{ props.handleTitleClick(e, props.video); }} >{props.video.snippet.title}</div>
       <div className="video-list-entry-detail">{props.video.snippet.description}</div>
     </div>
   </div>
